7f1989e3f164c16ca58acf74d6bf5688b52d41a8
Tamino <tamino@Mac.fritz.box>
2026-05-04 20:25:10 +0200

    Modify Configure Page

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "zh-CN",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", "**/.**"],
     defaultDateType: "modified",
