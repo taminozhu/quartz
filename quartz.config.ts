a0802efd93e2632ca76bb3187c0e7a693b560cd5
Tamino <tamino@Miless-MacBook-Pro.fritz.box>
2026-05-15 12:40:36 +0200

    忽略掉解析 README 文档

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "zh-CN",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", "**/.**"],
+    ignorePatterns: ["private", "templates", "**/.**", "README.md"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
